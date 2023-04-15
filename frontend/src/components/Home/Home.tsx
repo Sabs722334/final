d1b69bdee586aabfaf7d3435b77bf7a117dd30e3
aaziz001 <aziz2001900@gmail.com>
2023-04-15 13:01:51 +0500

    login page

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -8,11 +8,8 @@ interface HomePageProps {
   //you can always add more functions/objects that you would like as props for this component
 }
 
-function HomePage({}: HomePageProps) {
-  const [message, setMessage] = useState("");
-  const handleInputChange = (event: any) => {
-    setMessage(event.target.value);
-  };
+function HomePage(props: HomePageProps) {
+  const [signIn, setSignIn] = useState(true);
   async function handleClick() {
     try {
       const response = await axios.get("http://localhost:3000/");
@@ -23,15 +20,27 @@ function HomePage({}: HomePageProps) {
   }
   return (
     <>
-      <div className="sampleHomePage">
-        <h1 className="sampleTitle"></h1>
-        <div className="sampleMessage">
-          <button onClick={() => handleClick()}>
-            LOGIN
-          </button>
-          <button onClick={() => handleClick()}>
-            SIGNUP
-          </button>
+      <div className="homepage">
+        <div className="auth-window">
+          <h3 className="auth-title">{signIn ? "Login" : "Register"}</h3>
+          <form className="form">
+            <input className="input" type="text" placeholder="Username" />
+            <input className="input" type="password" placeholder="Password" />
+            <button className="btn-auth" type="submit" onClick={handleClick}>
+              {signIn ? "Login" : "Register"}
+            </button>
+          </form>
+          {signIn ? (
+            <p className="toggle-auth">
+              Don't have an account?{" "}
+              <span onClick={() => setSignIn(false)}>Register</span>
+            </p>
+          ) : (
+              <p className="toggle-auth">
+                Already have an account?{" "}
+                <span onClick={() => setSignIn(true)}>Login</span>
+            </p>
+          )}
         </div>
       </div>
     </>
